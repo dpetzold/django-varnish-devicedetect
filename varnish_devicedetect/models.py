2b473f9c2b01e1a9922ad191afcf95e1b45ec25b
Derrick Petzold <github@bzero.io>
2017-11-03 21:36:00 -0700

    ...

--- varnish_devicedetect/models.py
+++ varnish_devicedetect/models.py
@@ -2,13 +2,14 @@ class Device:
 
     def __init__(self, device_name):
         self.device_name = device_name
-        setattr(self, device_name.replace('-', '_'), True)
-        if '-' in device_name:
-            device_type, device_model = device_name.split('-')
+        normalized_name = device_name.replace('-', '_')
+        if '_' in normalized_name:
+            device_type, device_model = normalized_name.split('_')
             setattr(self, 'is_{}'.format(device_type), True)
             setattr(self, 'is_{}'.format(device_model), True)
         else:
             setattr(self, 'is_{}'.format(device_name), True)
+        setattr(self, normalized_name, True)
 
     def __str__(self):
         return self.device_name
